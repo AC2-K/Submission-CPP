c326f1f24397b6195e49fd6259e9e7d3f9c20872
AC2-K <117526989+AC2-K@users.noreply.github.com>
2023-01-05 18:24:56 +0900

    Solved EDPC/Q

--- Algo/JOI/main.cpp
+++ Algo/JOI/main.cpp
@@ -14,12 +14,8 @@ const ld eps = ld(0.000000001);
 const long double pi = 3.141592653589793;
 const ll MOD = 1e9 + 7;
 const ll MOD2 = 998244353;
-/*
 const int dx[4] = { 1,0,-1,0 };
 const int dy[4] = { 0,1,0,-1 };
-*/
-const int dx[]={1,1,1,0,0,-1,-1,-1,0};
-const int dy[]={1,0,-1,1,-1,1,0,-1,0};
 /*
 template<class T>using v=vector<T>;
 template<class T>using vv=v<v<T>>;
@@ -29,29 +25,4 @@ template<class T>using vvvv=v<vvv<T>>;
 template<class T>void chmax(T&x,T y){if(x<y)x=y;}
 template<class T>void chmin(T&x,T y){if(x>y)x=y;}
 int main() {
-    map<int,map<int,int>> cnt;
-    int h,w;
-    cin>>h>>w;
-    int n;
-    cin>>n;
-    vector<int> a(n),b(n);
-    rep(i,n){
-        cin>>a[i]>>b[i];
-        cnt[a[i]][b[i]]++;
-    }
-    int ans=0;
-    rep(i,n){
-        rep(ii,9){
-            int si=a[i]+dx[ii];
-            int sj=b[i]+dy[ii];
-            int res=0;
-            rep(iii,9){
-                int pi=si+dx[iii];
-                int pj=sj+dy[iii];
-                res+=cnt[pi][pj];
-            }
-            chmax(ans,res);
-        }
-    }
-    cout<<ans<<endl;
 }
