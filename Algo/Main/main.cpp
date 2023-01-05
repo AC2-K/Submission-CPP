3f3fc09e5e32ec2fc17180f8467eb3cf15daccb6
AC2-K <117526989+AC2-K@users.noreply.github.com>
2023-01-05 21:48:08 +0900

    Solved EDPC/P

--- Algo/Main/main.cpp
+++ Algo/Main/main.cpp
@@ -25,34 +25,4 @@ template<class T>using vvvv=v<vvv<T>>;
 template<class T>void chmax(T&x,T y){if(x<y)x=y;}
 template<class T>void chmin(T&x,T y){if(x>y)x=y;}
 int main() {
-    int n;
-    cin>>n;
-    vector<int> a(n);
-    for(auto&aa:a)cin>>aa;
-    graph g(n);
-    rep(i,n-1){
-        int u,v;
-        cin>>u>>v;
-        u--;
-        v--;
-        g[u].push_back(v);
-        g[v].push_back(u);
-    }
-    vector<int> dp(n+1,inf);   //dp[l]:=長さlのLISを作るとき、末尾の最小値
-    vector<int> ans(n);   //ans[v]:=0-vパス上のLIS
-    
-    auto dfs=[&](auto f,int v,int par=-1)-> void {
-        auto it=lower_bound(all(dp),a[v]);  //a[v]
-        ll pre=*it;
-        *it=a[v];
-        ans[v]=lower_bound(all(dp),inf)-dp.begin();
-        for(auto nex:g[v]){
-            if(nex==par)continue;
-            f(f,nex,v);
-        }
-        *it=pre;
-    };
-    dfs(dfs,0);
-
-    rep(v,n)cout<<ans[v]<<'\n';
 }
